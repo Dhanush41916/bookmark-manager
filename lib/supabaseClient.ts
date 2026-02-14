631506d5e56a85d85c0555e6c85e3587111d5fb1
Dhanush <petadhanush476@gmail.com>
2026-02-14 23:47:25 +0530

    Initial commit

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://endowhxaqlofdvhrablc.supabase.co',
+  'sb_publishable_pavCyW-jAJakVnX2MHr92Q_BlOD6NyX'
+)
